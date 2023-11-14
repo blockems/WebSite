017f5d2ea06fac9516616ee5d9ed3bd7a61199f3
Bruce Lock <block@as.net.au>
2023-11-14 15:16:20 +1100

    new site

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Hello World' });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
